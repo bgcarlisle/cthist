dd64133f314ae8f4f761980feec4193ea7635614
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2021-11-24 10:07:30 +0100

    Checks for already downloaded history data and starts test over

--- tests/testthat/test-clinicaltrials_gov_download.R
+++ tests/testthat/test-clinicaltrials_gov_download.R
@@ -1,3 +1,6 @@
 test_that("ClinicalTrials.gov history entries mass-download correctly", {
-  expect_equal(clinicaltrials_gov_download(c("NCT00942747", "NCT02110043"), "/tmp/history.csv"), TRUE)
+    if (file.exists("/tmp/history.csv")) {
+        file.remove("/tmp/history.csv")
+    }
+    expect_equal(clinicaltrials_gov_download(c("NCT00942747", "NCT02110043"), "/tmp/history.csv"), TRUE)
 })
