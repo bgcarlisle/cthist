40541c00599ea2258c644822a60c7f407e2a7204
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2021-11-24 09:41:29 +0100

    Added downloader for dates for DRKS history entries

---
+++ R/drks_de_version_dates.R
@@ -0,0 +1,59 @@
+#' Download a vector of dates on which a drks.de registry entry was
+#' updated
+#'
+#' @param drksid A character string including a well-formed DRKS id,
+#'     e.g. "DRKS00005219". (A capitalized "DRKS" followed by eight
+#'     numerals with no spaces or hyphens.)
+#'
+#' @return A vector of ISO-8601 formatted dates corresponding to the
+#'     dates on which there were clinical trial history version
+#'     updates.
+#'
+#' @export
+#'
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#' versions <- drks_de_version_dates("NCT00942747")
+#' 
+drks_de_version_dates <- function (drksid) {
+    
+    out <- tryCatch({
+
+        url <- paste0(
+            "https://drks.de/drks_web/navigate.do?navigationId=trial.history&TRIAL_ID=",
+            drksid
+        )
+
+        index <- rvest::read_html(url)
+
+        index %>%
+            rvest::html_nodes("tr:not(.bgHighlight) > td:nth-child(1)") %>%
+            rvest::html_text() %>%
+            as.Date(format="%m-%d-%Y") %>%
+            sort() %>%
+            format("%Y-%m-%d") %>%
+            return()
+
+    },
+    error=function(cond) {
+        message(paste("DRKS ID does not seem to exist:", drksid))
+        message("Here's the original error message:")
+        message(paste(cond, "\n"))
+                                        # Choose a return value in case of error
+        return ("Error")
+    },
+    warning=function(cond) {
+        message(paste("DRKS ID caused a warning:", drksid))
+        message("Here's the original warning message:")
+        message(paste(cond, "\n"))
+                                        # Choose a return value in case of warning
+        return("Warning")
+    },
+    finally={
+                                        # To execute regardless of success or failure
+    })
+    
+    return(out)
+    
+}
