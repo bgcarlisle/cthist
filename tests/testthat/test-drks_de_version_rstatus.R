1278e12872434c62154384f247c37461de418644
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2022-04-05 13:21:25 +0200

    Fix bug in DRKS rstatus test

--- tests/testthat/test-drks_de_version_rstatus.R
+++ tests/testthat/test-drks_de_version_rstatus.R
@@ -1,7 +1,7 @@
 test_that("DRKS recruitment status downloads correctly", {
-    version <- drks_de_version("NCT02110043", 1)
+    version <- drks_de_version("DRKS00015220", 1)
     expect_equal(
-        version$sponsor_data,
-        "Charite University, Berlin, Germany"
+        version$rstatus,
+        "Recruiting complete, follow-up complete"
     )
 })
