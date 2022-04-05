664bef95d9dd405d6758d8c5d490f5e5713ea968
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2022-04-05 13:21:41 +0200

    Rename ct dot gov gender-based test

--- tests/testthat/test-clinicaltrials_gov_version_gender_based.R
+++ tests/testthat/test-clinicaltrials_gov_version_gender_based.R
@@ -1,4 +1,4 @@
-test_that("ClinicalTrials.gov 'gender based' downloads correctly", {
+test_that("ClinicalTrials.gov gender-based downloads correctly", {
     version <- clinicaltrials_gov_version("NCT05306145", 1)
     expect_equal(
         version$gender_based,
