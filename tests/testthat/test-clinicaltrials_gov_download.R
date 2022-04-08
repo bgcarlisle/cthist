6a64f27c9f43debfd1f185a8211a44a9955bcccd
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2022-04-08 15:29:56 +0200

    Change to a trial with only one record

--- tests/testthat/test-clinicaltrials_gov_download.R
+++ tests/testthat/test-clinicaltrials_gov_download.R
@@ -4,7 +4,7 @@ test_that("ClinicalTrials.gov history entries mass-download correctly", {
         file.remove(filename)
     }
     result <- clinicaltrials_gov_download(
-        c("NCT00942747"), filename, TRUE
+        c("NCT02329873"), filename, TRUE
     )
     expect_equal(
         result == TRUE || result == FALSE,
