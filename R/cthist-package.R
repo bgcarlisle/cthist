d64b5cde040f16254c49344c1bbed5733cf27785
Benjamin Gregory Carlisle PhD <bgcarlisle@users.noreply.github.com>
2022-12-11 11:41:52 -0500

    Update package documentation to remove DRKS

--- R/cthist-package.R
+++ R/cthist-package.R
@@ -1,7 +1,6 @@
-#' @details This package provides 6 functions for mass-downloading
-#'     historical clinical trial registry entry data, 3 for
-#'     ClinicalTrials.gov and 3 for
-#'     DRKS.de.
+#' @details This package provides 3 functions for mass-downloading
+#'     historical clinical trial registry entry data from
+#'     ClinicalTrials.gov
 #'
 #' @details clinicaltrials_gov_dates() downloads the dates on which
 #'     clinical trial registry entries were updated from
@@ -14,18 +13,6 @@
 #' @details clinicaltrials_gov_download() mass-downloads clinical
 #'     trial registry entry versions for one or many trials on
 #'     ClinicalTrials.gov
-#'
-#' @details drks_de_dates() downloads the dates on which
-#'     clinical trial registry entries were updated from
-#'     DRKS.de
-#'
-#' @details drks_de_version() downloads a specified
-#'     historical version of a clinical trial registry entry from
-#'     DRKS.de
-#'
-#' @details drks_de_download() mass-downloads clinical
-#'     trial registry entry versions for one or many trials on
-#'     DRKS.de
 #'
 #' @references Carlisle, BG. Analysis of clinical trial registry entry
 #'     histories using the novel R package cthist. medRxiv, 2022. doi:
