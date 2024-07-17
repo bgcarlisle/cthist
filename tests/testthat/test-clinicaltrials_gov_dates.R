e98385085675884d750a5783f0310336b957df00
Benjamin G Carlisle PhD <bgcarlisle@users.noreply.github.com>
2024-07-17 10:48:06 -0400

    Making the test a bit more specific

--- tests/testthat/test-clinicaltrials_gov_dates.R
+++ tests/testthat/test-clinicaltrials_gov_dates.R
@@ -1,7 +1,7 @@
 test_that("ClinicalTrials.gov version dates download correctly", {
     result <- clinicaltrials_gov_dates("NCT00942747")
     expect_equal(
-        "2013-04-15" %in% result$version_date || result == "Error",
+        "2013-04-15" == result$version_date[3] || result == "Error",
         TRUE
     )
 })
