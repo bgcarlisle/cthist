307e1c7db28439d0aac68f80e13a0affc48c9823
BG Carlisle <bgcarlisle@users.noreply.github.com>
2025-08-29 08:23:15 -0400

    Update unit tests to reflect minor changes in formatting on server

--- tests/testthat/test-overall_status_lengths.R
+++ tests/testthat/test-overall_status_lengths.R
@@ -1,19 +1,21 @@
 test_that("Overall status lengths parse correctly", {
-    versions <- clinicaltrials_gov_download("NCT04338971")
-    oslengths <- overall_status_lengths(
-        versions,
-        start_date = "2020-01-01",
-        end_date = "2021-10-31"
-    )
-    completed <- oslengths %>%
-        dplyr::filter(overall_status == "COMPLETED") %>%
-        dplyr::pull(days) %>%
-        sum() %>%
-        as.numeric()
+  versions <- clinicaltrials_gov_download(
+    "NCT04338971", quiet = TRUE
+  )
+  oslengths <- overall_status_lengths(
+    versions,
+    start_date = "2020-01-01",
+    end_date = "2021-10-31"
+  )
+  completed <- oslengths %>%
+    dplyr::filter(overall_status == "COMPLETED") %>%
+    dplyr::pull(days) %>%
+    sum() %>%
+    as.numeric()
 
-    expect_equal(
-        completed,
-        62
-    )
+  expect_equal(
+    completed,
+    62
+  )
 
 })
